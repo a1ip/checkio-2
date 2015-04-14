bd3cee4b4e5b5c232aa8d3f27770d25dd483389a
Anton Rasmussen <anton.j.rasmussen@gmail.com>
2015-04-14 09:49:47 -0500

    Update non_unique.py
    cleaning things up

--- non_unique.py
+++ non_unique.py
@@ -21,20 +21,9 @@ def checkio():
 
 
 
+# checkio solution I made (that worked):
 
-checkio()
-
-
-''' checkio solution I made (that worked):
-
-#Your optional code here
-#You can import some modules or create additional functions
-
-
-def checkio(data):
-    #Your code here
-    #It's main function. Don't remove this function
-    #It's used for auto-testing and must return a result for check.  
+def non_unique(data):
 
     #user_input = [int(x) for x in data.split()]
     new_data = []
@@ -43,18 +32,3 @@ def checkio(data):
             new_data.append(element) 
     return new_data
 
-#Some hints
-#You can use list.count(element) method for counting.
-#Create new list with non-unique elements
-#Loop over original list
-
-
-if __name__ == "__main__":
-    #These "asserts" using only for self-checking and not necessary for auto-testing
-    assert isinstance(checkio([1]), list), "The result must be a list"
-    assert checkio([1, 2, 3, 1, 3]) == [1, 3, 1, 3], "1st example"
-    assert checkio([1, 2, 3, 4, 5]) == [], "2nd example"
-    assert checkio([5, 5, 5, 5, 5]) == [5, 5, 5, 5, 5], "3rd example"
-    assert checkio([10, 9, 10, 10, 9, 8]) == [10, 9, 10, 10, 9], "4th example"
-
-'''

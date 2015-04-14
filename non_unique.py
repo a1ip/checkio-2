9bd243f173a0e46b86e6bf1c32253bbc070b01b0
Anton Rasmussen <anton.j.rasmussen@gmail.com>
2015-04-14 09:54:54 -0500

    Update non_unique.py
    clean things up

--- non_unique.py
+++ non_unique.py
@@ -1,15 +1,30 @@
-def checkio():
-    '''
-    You are given a non-empty list of integers (X).
-    For this task, you should return a list consisting of only the non-unique elements
-    in this list. To do so you will need to remove all unique elements
-    (elements which are contained in a given list only once).
-    When solving this task, do not change the order of the list.
-    Example: [1, 2, 3, 1, 3] 1 and 3 non-unique elements and result will be [1, 3, 1, 3].
-    
-    Source: http://www.tutorialspoint.com/python/python_lists.htm
-    '''
+'''
+You are given a non-empty list of integers (X). For this task, you should return a list consisting of only the non-unique elements in this list. To do so you will need to remove all unique elements (elements which are contained in a given list only once). When solving this task, do not change the order of the list. Example: [1, 2, 3, 1, 3] 1 and 3 non-unique elements and result will be [1, 3, 1, 3].
 
+non-unique-elements
+
+Input: A list of integers.
+
+Output: The list of integers.
+
+Example:
+
+checkio([1, 2, 3, 1, 3]) == [1, 3, 1, 3]
+checkio([1, 2, 3, 4, 5]) == []
+checkio([5, 5, 5, 5, 5]) == [5, 5, 5, 5, 5]
+checkio([10, 9, 10, 10, 9, 8]) == [10, 9, 10, 10, 9]
+
+How it is used: This mission will help you to understand how to manipulate arrays, something that is the basis for solving more complex tasks. The concept can be easily generalized for real world tasks. For example: if you need to clarify statistics by removing low frequency elements (noise).
+
+Precondition:
+0 < len(data) < 1000
+
+'''
+
+
+
+def checkio():
+    # Source: http://www.tutorialspoint.com/python/python_lists.htm
     print('Please enter some numbers (separated by a space): ')
     user_input = [int(x) for x in input().split()] # Source: http://stackoverflow.com/questions/4663306/how-can-i-get-a-list-as-input-from-the-user-in-python
     print('Your list is: ', user_input)
